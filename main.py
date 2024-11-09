6dd1547d212bf97f9c53548a8260eec966396fbb
Hayyan67 <humairabaig81@gmail.com>
2024-11-09 14:50:33 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+class myClass:
+
+
+    _privateVar = 27
+
+
+    def _privMeth(self):
+        print("I am inside class myClass")
+
+
+        def hello(self):
+            print("Private Variable value: ",myClass._privateVar)
+
+foo = myClass()
+foo.hello()
+foo._privMeth
